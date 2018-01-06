3b3cd40b08b42488e706fd4c9724474b59f27358
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2018-01-06 18:17:55 +0100

    Add index.html & ctalogs with files style.css & scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+var a = 1;
+var h = 2;
+var triangleArea = a * h / 2;
+
+/* Tak jak poniżej też można?
+var a = 1 , h = 2 , triangleArea = a * h / 2;
+*/
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
